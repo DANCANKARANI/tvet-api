6cfd16a2f397be0c3d22cbf40235007f5c804ab5
DANCAN KARANI <karanidancan120@gmail.com>
2024-08-21 23:00:24 +0300

    cors

--- api/routes/students/route.go
+++ api/routes/students/route.go
@@ -8,9 +8,10 @@ import (
 
 func SetStudentRoutes(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
-        AllowOrigins: "http://localhost:3000, http://127.0.0.1",
-        AllowHeaders: "Origin, Content-Type, Accept",
-    }))
+		AllowOrigins: "*", // Allow all origins
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	
 	auth := app.Group("/api/v1/student")
 	auth.Get("/all",student.GetAllStudent)
 	auth.Post("/register",student.CreateStudentAccount)
