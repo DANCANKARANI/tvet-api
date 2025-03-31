f44f65ffb183a0ef51a0bb659f0ab7aae7888267
DANCAN KARANI <karanidancan120@gmail.com>
2025-04-01 00:29:30 +0300

    my changes

--- api/endpoints/endpoints.go
+++ api/endpoints/endpoints.go
@@ -16,7 +16,7 @@ func CreateEndpoint() {
 	app := fiber.New()
 	
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "http://localhost:3000, https://tvetidris.vercel.app", // Added the Vercel URL
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With",
 		AllowCredentials: true,
